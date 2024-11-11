a1a467e47bc984510150d242680280be25697a40
shejanNizam <nizam.shejan11@gmail.com>
2024-11-11 21:10:47 +0600

    banner

--- src/components/HomePage/Banner.tsx
+++ src/components/HomePage/Banner.tsx
@@ -1,10 +1,9 @@
 export default function Banner() {
   return (
     <div>
-      <h2> This is Banner </h2>
-      <h2> This is Banner </h2>
-      <h2> This is Banner </h2>
-      <h2> This is Banner </h2>
+      <h3 className="text-center text-4xl font-semibold text-purple-600 my-8">
+        Banner section
+      </h3>
     </div>
   );
 }
