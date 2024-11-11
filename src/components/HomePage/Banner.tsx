52eda22489bcce0edb9eac91652028e0683bd49f
shejanNizam <nizam.shejan11@gmail.com>
2024-11-11 12:27:49 +0600

    test

--- src/components/HomePage/Banner.tsx
+++ src/components/HomePage/Banner.tsx
@@ -2,6 +2,9 @@ export default function Banner() {
   return (
     <div>
       <h2> This is Banner </h2>
+      <h2> This is Banner </h2>
+      <h2> This is Banner </h2>
+      <h2> This is Banner </h2>
     </div>
   );
 }
