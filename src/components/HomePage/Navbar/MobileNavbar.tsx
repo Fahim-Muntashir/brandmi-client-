0a8abdce681b5b2a6e9218d9c8389122afd0f492
AHFahim009 <ahfba009@gmail.com>
2024-11-11 13:33:52 +0600

    Add navbar

---
+++ src/components/HomePage/Navbar/MobileNavbar.tsx
@@ -0,0 +1,61 @@
+import { Button } from "@/components/ui/button";
+import {
+  Sheet,
+  SheetContent,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+import { Menu } from "lucide-react";
+import Link from "next/link";
+import { useState } from "react";
+import { navItems } from "./Navbar";
+
+const MobileNavbar = () => {
+  const [onOpen, setOnOpen] = useState(false);
+  const handleMobileMenu = () => {
+    setOnOpen(true);
+  };
+  return (
+    <Sheet open={onOpen} onOpenChange={setOnOpen}>
+      <SheetTrigger asChild>
+        <Button
+          variant="outline"
+          size="icon"
+          onClick={handleMobileMenu}
+          className=" shrink-0 px-0 text-base hover:bg-transparent focus-visible:bg-transparent focus-visible:ring-0 focus-visible:ring-offset-0 md:hidden"
+        >
+          <Menu className="h-5 w-5" />
+          <span className="sr-only">Toggle Menu</span>
+        </Button>
+      </SheetTrigger>
+      <SheetContent
+        side="left"
+        className="px-4 h-screen overflow-y-auto flex flex-col items-center  "
+      >
+        <SheetHeader className="mb-4 mt-8">
+          <SheetTitle className="flex gap-2 ">
+            <Link href="/" className=" flex items-center space-x-2">
+              <span className="font-bold text-2xl ">Our logo</span>
+            </Link>
+          </SheetTitle>
+        </SheetHeader>
+        <ul className="flex flex-col gap-2">
+          {navItems.map((item) => {
+            return (
+              <li key={item.href}>
+                <Link
+                  href={item.href}
+                  className={`flex text-xl items-center scroll-m-20  font-medium tracking-tight`}
+                >
+                  {item.label}
+                </Link>
+              </li>
+            );
+          })}
+        </ul>
+      </SheetContent>
+    </Sheet>
+  );
+};
+export default MobileNavbar;
---
+++ src/components/HomePage/Navbar/Navbar.tsx
@@ -0,0 +1,59 @@
+"use client";
+import Link from "next/link";
+
+import ToggleMode from "./ToogleMode";
+import MobileNavbar from "./MobileNavbar";
+import UserMenu from "./UserMenu";
+import { usePathname } from "next/navigation";
+
+export const navItems = [
+  { href: "/", label: "Home" },
+  { href: "/profile", label: "Profile" },
+  { href: "/settings", label: "Settings" },
+  { href: "/dashboard", label: "dashboard" },
+];
+
+export default function Navbar() {
+  const pathname = usePathname();
+  return (
+    <main>
+      <header className=" w-full border-b  ">
+        <div className="container mx-auto px-4 flex h-14 items-center">
+          {/* logo  */}
+          <div className="mr-4 hidden md:flex">
+            <Link href="/" className="mr-6 flex items-center space-x-2">
+              <span className="hidden font-bold sm:inline-block text-xl">
+                Our logo
+              </span>
+            </Link>
+          </div>
+          {/* mobile view */}
+          <MobileNavbar />
+          {/* left side */}
+          <div className="flex flex-1 items-center  space-x-6 justify-end">
+            {/* desktop nav links */}
+            <div className="items-center space-x-4 hidden md:block">
+              {navItems.map((item) => (
+                <Link
+                  key={item.label}
+                  href={item.href}
+                  className={`text-sm font-medium  hover:text-primary ${
+                    pathname === item.href
+                      ? "text-primary"
+                      : "text-muted-foreground"
+                  }`}
+                >
+                  {item.label}
+                </Link>
+              ))}
+            </div>
+            <div className="flex items-center gap-4">
+              <UserMenu />
+              <ToggleMode />
+            </div>
+          </div>
+        </div>
+      </header>
+    </main>
+  );
+}
