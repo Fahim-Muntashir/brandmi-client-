866da4d2a1f90adaa616af5d70be894f7f4a7f66
FahimMuntashir1 <fahimasdf07@gmail.com>
2024-11-11 12:51:17 +0600

    Added footer

--- src/app/(withcommonlayout)/page.tsx
+++ src/app/(withcommonlayout)/page.tsx
@@ -1,8 +1,12 @@
+import Footer from "@/components/HomePage/Footer";
+
 const HomePage = () => {
   return (
     <>
       Hello For Home
       <h2> Banner from nizam </h2>
+
+      <Footer></Footer>
     </>
   );
 };
