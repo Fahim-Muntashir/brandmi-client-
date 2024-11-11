d84d547b68034cd00801cdbac0b39e19133ce50e
FahimMuntashir1 <fahimasdf07@gmail.com>
2024-11-11 13:04:35 +0600

    added

--- src/app/(withcommonlayout)/page.tsx
+++ src/app/(withcommonlayout)/page.tsx
@@ -1,14 +1,13 @@
 import Footer from "@/components/HomePage/Footer";
 
-const HomePage = () => {
-  return (
-    <>
-      Hello For Home
-      <h2> Banner from nizam </h2>
 
-      <Footer></Footer>
-    </>
-  );
+
+const HomePage = () => {
+    return (
+        <>
+            <Footer>
+            </Footer>        </>
+    );
 };
 
 export default HomePage;
