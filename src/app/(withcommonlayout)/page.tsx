687f7174346952fb8d90b28703c013740c0f9445
FahimMuntashir1 <fahimasdf07@gmail.com>
2024-11-12 19:59:21 +0600

    added

--- src/app/(withcommonlayout)/page.tsx
+++ src/app/(withcommonlayout)/page.tsx
@@ -1,21 +1,21 @@
 import Category from "@/components/HomePage/category/Category";
-import Banner from "@/components/HomePage/Banner";
+import Banner from "@/components/HomePage/Banner/Banner";
 import Footer from "@/components/HomePage/Footer/Footer";
 import Navbar from "@/components/HomePage/Navbar/Navbar";
 import PopularServices from "@/components/HomePage/PopularServices/TrendingOffers";
 
 const HomePage = () => {
-  return (
-    <>
-      <div className="bg-[#D1D6E2]">
-        <Navbar />
-        <Banner />
-      </div>
-      <Category />
-      <PopularServices />
-      <Footer />
-    </>
-  );
+    return (
+        <>
+            <div className="bg-[#D1D6E2]">
+                <Navbar />
+                <Banner />
+            </div>
+            <Category />
+            <PopularServices />
+            <Footer />
+        </>
+    );
 };
 
 export default HomePage;
