18e546a2fbdc4cbb3499d756c6d4f8350ee70784
AHFahim009 <ahfba009@gmail.com>
2024-11-15 01:49:40 +0600

    add Dashboard layout

---
+++ src/layouts/Dashboard.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import Header from "./Header";
+import Sidebar from "./Sidebar";
+
+interface DashboardProps {
+  children: React.ReactNode;
+}
+const Dashboard = ({ children }: DashboardProps) => {
+  return (
+    <div className="flex h-screen max-w-[1400px] mx-auto">
+      {/* sidebar */}
+      <Sidebar />
+
+      {/* right area */}
+      <div className="flex flex-col flex-1 overflow-hidden ">
+        {/* header */}
+        <Header />
+        <main className="flex-1 overflow-y-auto overflow-x-hidden">
+          <div className="w-full h-full  px-6 py-6 ">{children}</div>
+        </main>
+      </div>
+    </div>
+  );
+};
+export default Dashboard;
