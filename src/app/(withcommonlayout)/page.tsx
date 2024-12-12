7b244886e17ac6384760fc5e24c50025e906d97d
AHFahim009 <ahfba009@gmail.com>
2024-12-12 17:31:05 +0600

    add one tap google login

--- src/app/(withcommonlayout)/page.tsx
+++ src/app/(withcommonlayout)/page.tsx
@@ -5,20 +5,21 @@ import Navbar from "@/components/HomePage/Navbar/Navbar";
 import PopularServices from "@/components/HomePage/PopularServices/TrendingOffers";
 import BusinessSolution from "@/components/HomePage/BusinessSolution/BusinessSolution";
 import Members from "@/components/members/Members";
+import GoogleOneTapLogin from "@/auth/GoogleOneTap";
 
 const HomePage = () => {
-    return (
-        <>
-            <Navbar />
-            <Banner />
-            <Category />
-            <PopularServices />
-            <BusinessSolution />
-            <Members />
-            <Footer />
-        </>
-    );
+  return (
+    <>
+      <GoogleOneTapLogin />
+      <Navbar />
+      <Banner />
+      <Category />
+      <PopularServices />
+      <BusinessSolution />
+      <Members />
+      <Footer />
+    </>
+  );
 };
 
 export default HomePage;
-
