93225f3bc2db3ede30285372a998a16ea48ffc49
FahimMuntashir1 <fahimasdf07@gmail.com>
2024-12-19 19:28:54 +0600

    added

--- src/app/me/page.tsx
+++ src/app/me/page.tsx
@@ -1,7 +1,28 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import axiosInstance, { ErrorResponse, ResponseOptions } from "@/axios/axios";
 import Navbar from "@/components/HomePage/Navbar/Navbar";
-import React from "react";
+import { cookies } from "next/headers";
 
-const Profile: React.FC = () => {
+const Profile: React.FC = async () => {
+  const cookieStore = await cookies();
+  const accessToken = cookieStore.get("accessToken")?.value || null;
+  const refreshToken = cookieStore.get("refreshToken")?.value || null;
+  let data;
+  try {
+    const res = await axiosInstance.get("/user/me", {
+      headers: {
+        // Forward cookies manually
+        Cookie: `accessToken=${accessToken}; refreshToken=${refreshToken}`,
+      },
+    });
+    const result = res.data as ResponseOptions<any>;
+    data = result.data;
+    //   window.location.href = "/";
+  } catch (error) {
+    const err = error as ErrorResponse;
+    data = err.message;
+    console.log(data);
+  }
   return (
     <div>
       <Navbar></Navbar>
